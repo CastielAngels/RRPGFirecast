eaee20a3ee2059d052f0de5421fcb8cdb9ed965a
CastielAngels <40968236+CastielAngels@users.noreply.github.com>
2019-07-03 22:32:17 -0300

    #019

--- Plugin/IMGDroid/Main.lua
+++ Plugin/IMGDroid/Main.lua
@@ -1,10 +1,15 @@
 -- Script desenvolvido por CastielAngels E-Mail: castieldeseraphim@gmail.com --
 require("rrpg.lua");
+require("gui.lua");
+require("system.lua"); 
 
 rrpg.messaging.listen("HandleChatCommand",
 	function(message)
 		if message.comando == "i" then
-			message.chat:enviarMensagem("[§I" .. message.parametro .. "]");
+			--message.chat:enviarMensagem("[§I" .. message.parametro .. "]");
+			
+			local imgdroidform = gui.showPopup("imgdroidform");
+			
 			message.response = {handled = true};
 		end;
 	end);
